9c1c80f7111737f233c4b6ac1ee0879af319dfc8
Seyi MontXero <a.arogbonlo@gmail.com>
2018-06-10 15:10:19 +1000

    initial commit

---
+++ normalise_names.py
@@ -0,0 +1,46 @@
+#! /bin/bash/python
+
+""" normalise_names.py
+This program is designed to normalise the file names of assessment submissions to
+Cloud Deakin.
+It is runs in a directory containing the file names and converts all pdf and
+docx files as follows:
+    infile::
+    000000-133333 - foon - FOOO NOOOOON - 999999991 - 29 May, 2018 4444 PM - Final Report.pdf
+    outfile::
+    fooo_nooooon.pdf
+
+Author: Xero
+Licence: GPL v3
+"""
+
+from __future__ import print_function
+from os import rename, listdir
+from os.path import isfile, join
+
+def is_interesting(somefile,  filetypes=("pdf", "docx", "doc")):
+    return any(somefile.endswith(ending) or somefile.endswith(ending.upper())
+                for ending in filetypes)
+
+def get_files_of_interest(path):
+    files = filter(lambda f: isfile(join(path, f)), listdir(path))
+    return filter(is_interesting, files) 
+
+
+def normalise_names(files):
+    i = 0
+    for f in files:
+        print("processing {}".format(f))
+        try:
+            person_name = f.split("-")[2]
+            new_name = person_name.strip().lower().replace(" ", "_")
+            rename(f, new_name)
+            i += 1
+        except Exception as e:
+            print("Skiping {} because of {}".format(f, e))
+    print("Done. processed {} files.".format(i))
+
+
+if __name__ == "__main__":
+    base_dir = "./"
+    normalise_names(get_files_of_interest(base_dir))
